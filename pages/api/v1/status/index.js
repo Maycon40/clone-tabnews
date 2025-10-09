310153b45b1f1e2bb352825a8c8da45f0cfe80f8
Maycon40 <71737549+Maycon40@users.noreply.github.com>
2025-10-09 18:07:22 +0000

    finish the endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  console.log(database);
-  response.status(200).json({ maycon: "maycon é bonito" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+  const usedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const versionResult = await database.query("SHOW server_version;");
+
+  const { server_version: version } = versionResult.rows[0];
+
+  const { max_connections: maxConnections } = maxConnectionsResult.rows[0];
+
+  const { count: usedConnections } = usedConnectionsResult.rows[0];
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version,
+        max_connections: Number(maxConnections),
+        used_connections: usedConnections,
+      },
+    },
+  });
 }
 
 export default status;
