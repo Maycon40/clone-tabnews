ba334dabecc51a2240c492c60b01b9fd06da8886
Maycon Santos <71737549+Maycon40@users.noreply.github.com>
2026-01-23 19:12:23 +0000

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -4,9 +4,13 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Testing method not allowed", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+      expect(response.status).toBe(405);
+    });
   });
-  expect(response.status).toBe(405);
 });
