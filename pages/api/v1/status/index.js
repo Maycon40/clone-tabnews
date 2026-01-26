efd9983b1e75c9578e32685972b8f3c4bec3eee5
Maycon Santos <71737549+Maycon40@users.noreply.github.com>
2026-01-26 18:09:06 +0000

    fix: make /api/v1/status endpoint return an Internal Server Error when occurs a exception

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,33 +1,47 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const databaseName = process.env.POSTGRES_DB;
+    const databaseName = process.env.POSTGRES_DB;
 
-  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
-  const usedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const versionResult = await database.query("SHOW server_version;");
+    const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+    const usedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const versionResult = await database.query("SHOW server_version;");
 
-  const { server_version: version } = versionResult.rows[0];
+    const { server_version: version } = versionResult.rows[0];
 
-  const { max_connections: maxConnections } = maxConnectionsResult.rows[0];
+    const { max_connections: maxConnections } = maxConnectionsResult.rows[0];
 
-  const { count: usedConnections } = usedConnectionsResult.rows[0];
+    const { count: usedConnections } = usedConnectionsResult.rows[0];
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version,
-        max_connections: Number(maxConnections),
-        used_connections: usedConnections,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version,
+          max_connections: Number(maxConnections),
+          used_connections: usedConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    console.log("\n Erro dentro do catch do controller:");
+    console.error(error);
+
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.error(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
