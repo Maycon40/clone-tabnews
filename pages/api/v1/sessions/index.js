0c6d3d2881d3419aa01cb5fd0ed3181d597fe86d
Maycon <mayconprogramador@gmail.com>
2026-01-30 20:55:55 +0000

    feat: add cookie for session and add the tests

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -23,7 +23,9 @@ async function postHandler(request, response) {
 
   const setCookie = cookie.serialize("session_id", newSession.token, {
     path: "/",
-    expires: newSession.expires_at,
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
   });
 
   response.setHeader("Set-Cookie", setCookie);
