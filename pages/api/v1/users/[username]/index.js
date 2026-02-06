f2b44a36140a497881bfd6a863837ac507e8a9b2
Maycon <mayconprogramador@gmail.com>
2026-02-06 20:38:06 +0000

    feat: require authorization to PATCH /api/v1/users/[username] endpoint

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,14 +1,14 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user";
+import { ForbiddenError } from "infra/errors";
 
 const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
 
-//router.get(controller.canRequest("read:session"), getHandler);
 router.get(getHandler);
-router.patch(patchHandler);
+router.patch(controller.canRequest("update:user"), patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -22,6 +22,16 @@ async function getHandler(request, response) {
 async function patchHandler(request, response) {
   const username = request.query.username;
   const userInputValues = request.body;
+  const currentUser = request.context.user;
+
+  const userToUpdate = await user.findOneByUsername(username);
+
+  if (userToUpdate.id != currentUser.id) {
+    throw new ForbiddenError({
+      message: "Você não tem permissão para alterar outros usuários.",
+      action: "Utilize seu nome de usuário para alterar as informações.",
+    });
+  }
 
   const updatedUser = await user.updateByUsername(username, userInputValues);
 
